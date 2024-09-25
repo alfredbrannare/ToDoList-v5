96e237872e2a07ae94455097e643e2c48ea2909d
Alfred <alfred-brannarer@hotmail.com>
2024-09-25 12:45:09 +0200

    Redid my previous To-Do List to refresh what I originally had done. This one is without the CSS amount of the previous one.

---
+++ main.js
@@ -0,0 +1,104 @@
+// JS for the To-Do List
+
+// Declaring variables
+const taskStorage = [];
+const finishedStorage = [];
+const taskInfo = document.querySelector("#taskInfo");
+const taskTextInput = document.querySelector("#taskTextInput");
+const addTaskBtn = document.querySelector("#addTaskBtn");
+const taskBox = document.querySelector("ul");
+const finishedTask = document.querySelector("#finishedTask");
+const remainingTask = document.querySelector("#remainingTask");
+const warning = document.querySelector("#warning");
+
+let taskId = 1;
+
+addTaskBtn.addEventListener("click", function () {
+    // Alert if input is empty
+    if (taskTextInput.value === "") {
+        warning.innerHTML = "You have to type something";
+        return;
+    } else {
+        warning.innerHTML = "";
+    }
+
+    // Declaring a variable for the user input
+    const newTask = taskTextInput.value;
+
+    // Object managing tasks
+    const taskObject = { id: taskId++, text: newTask, completed: false };
+
+    // Create the li
+    const createLi = document.createElement("li");
+    createLi.classList.add("liList");
+    taskBox.appendChild(createLi);
+
+    // Create the span for the text input
+    const createSpan = document.createElement("span");
+    createSpan.innerHTML = taskObject.text;
+    createLi.appendChild(createSpan);
+
+    taskTextInput.value = "";
+
+    // Push taskObject to taskStorage
+    taskStorage.push(taskObject);
+    console.log(taskStorage);
+
+    // Span click event for marking tasks
+    createSpan.addEventListener("click", function () {
+        taskObject.completed = !taskObject.completed;
+
+        // Move task between taskStorage and finishedStorage
+        if (taskObject.completed) {
+            createSpan.classList.add("markCompl");
+            const indexTwo = taskStorage.indexOf(taskObject);
+            if (indexTwo > -1) {
+                taskStorage.splice(indexTwo, 1);
+                finishedStorage.push(taskObject);
+                console.log(finishedStorage);
+            }
+        } else {
+            createSpan.classList.remove("markCompl");
+            const indexOne = finishedStorage.indexOf(taskObject);
+            if (indexOne > -1) {
+                finishedStorage.splice(indexOne, 1);
+                taskStorage.push(taskObject);
+                console.log(finishedStorage);
+            }
+        }
+        taskCount();
+    });
+
+    // Delete button
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = "&#128465;";
+    delBtn.classList.add("delBtn");
+    createLi.appendChild(delBtn);
+
+    // Delete button event listener
+    delBtn.addEventListener("click", function () {
+        if (taskObject.completed) {
+            // Find and remove from finishedStorage
+            const indexTwo = finishedStorage.indexOf(taskObject);
+            if (indexTwo > -1) {
+                finishedStorage.splice(indexTwo, 1);
+            }
+        } else {
+            // Find and remove from taskStorage
+            const indexOne = taskStorage.indexOf(taskObject);
+            if (indexOne > -1) {
+                taskStorage.splice(indexOne, 1);
+            }
+        }
+        createLi.remove();  
+        taskCount();        
+    });
+
+    taskCount();
+});
+
+// Task count function
+function taskCount() {
+    finishedTask.textContent = `Finished Tasks: ${finishedStorage.length}`;
+    remainingTask.textContent = `Remaining Tasks: ${taskStorage.length}`;
+}
