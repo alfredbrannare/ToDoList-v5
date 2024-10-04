ebb0e70933105fc17117c04cd7c798fea2bb6c4e
Alfred <alfred-brannarer@hotmail.com>
2024-10-04 15:55:21 +0200

    Minor changes to get an animation working

--- main.js
+++ main.js
@@ -12,14 +12,22 @@ const remainingTask = document.querySelector("#remainingTask");
 const warning = document.querySelector("#warning");
 const themesBtn = document.querySelector("#themesBtn");
 const themeStylesheet = document.querySelector("#themeStylesheet");
+const taskInputs = document.querySelector("#taskInputs");
 
 let taskId = 1;
 
 addTaskBtn.addEventListener("click", function () {
     // Alert if input is empty
     if (taskTextInput.value === "") {
-        warning.style.animation = "fadeIn 0.5s ease forwards";
-        warning.innerHTML = "You have to type something";
+        warning.innerHTML = "Input must not be empty";
+
+        //Only solution I could find to fix the empty input animation text
+        warning.classList.add("warningAnimation");
+
+        warning.addEventListener("animationend", function(){
+            warning.classList.remove("warningAnimation");
+        }, {once: true});
+
         return;
     } else {
         warning.innerHTML = "";
@@ -39,7 +47,7 @@ addTaskBtn.addEventListener("click", function () {
     // Create the span for the text input
     const createSpan = document.createElement("span");
     createSpan.innerHTML = taskObject.text;
-    createSpan.classList.add("spanList")
+    createSpan.classList.add("spanList");
     createLi.appendChild(createSpan);
 
     // Push taskObject to taskStorage
@@ -63,45 +71,45 @@ addTaskBtn.addEventListener("click", function () {
 
     // Delete button
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = "x";
+    delBtn.innerHTML = "🗑️";
     delBtn.classList.add("delBtn");
     createLi.appendChild(delBtn);
 
     // Delete button event listener
     delBtn.addEventListener("click", function () {
-        createLi.style.animation = "fadeOut 0.5s ease forwards";
-
-        setTimeout(function () {
             const indexOne = taskStorage.indexOf(taskObject);
             if (indexOne > -1) {
                 taskStorage.splice(indexOne, 1);
                 createLi.remove();
                 taskCount();
             }
-        }, 500);
+        });
 
     });
     taskCount();
-});
+
 
 // Task count function
 function taskCount() {
     const finishedTasksCount = taskStorage.filter(task => task.completed).length;
     const remainingTaskCount = taskStorage.filter(task => !task.completed).length;
 
-    finishedTask.textContent = `Finished Tasks: ${finishedTasksCount}`;
-    remainingTask.textContent = `Remaining Tasks: ${remainingTaskCount}`;
+    finishedTask.textContent = `${finishedTasksCount} completed`;
+
+    // remainingTask.textContent = `Remaining Tasks: ${remainingTaskCount}`;
 }
 
-themesBtn.addEventListener("click", function(){
+
+//SCRAPPED FROM MY ORIGINAL TODO LIST WILL CONTINUE IN THE FUTURE
+// themesBtn.addEventListener("click", function(){
     
-    if (themesBtn.textContent === "Modern"){
-        themesBtn.textContent = "Fantasy";
-        themeStylesheet.href = "main.css";
-        console.log("Switched to Modern theme");        
-    } else if (themesBtn.textContent == "Fantasy"){
-        themesBtn.textContent = "Modern";
-        themeStylesheet.href = "main2.css";
-        console.log("Switched to Fantasy theme");
-    }
-})
+//     if (themesBtn.textContent === "Fantasy"){
+//         themesBtn.textContent = "Modern";
+//         themeStylesheet.href = "main2.css";
+//         console.log("Switched to Fantasy theme");        
+//     } else if (themesBtn.textContent == "Modern"){
+//         themesBtn.textContent = "Fantasy";
+//         themeStylesheet.href = "main.css";
+//         console.log("Switched to Modern theme");
+//     }
+// })
